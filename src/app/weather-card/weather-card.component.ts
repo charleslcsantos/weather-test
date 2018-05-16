a35cb81ddee3086ecc3e2eeba1b5d29411fa7635
Charles Lennon <charleslcsantos@gmail.com>
2018-05-16 17:27:08 -0300

    layout finalizado

--- src/app/weather-card/weather-card.component.ts
+++ src/app/weather-card/weather-card.component.ts
@@ -1,15 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, Input, ViewEncapsulation } from '@angular/core';
+
+enum WEATHER_TEMP_STATES {
+  COLD = 'cold',
+  HOT = 'hot',
+  REGULAR = 'regular'
+}
 
 @Component({
   selector: 'app-weather-card',
   templateUrl: './weather-card.component.html',
-  styleUrls: ['./weather-card.component.scss']
+  styleUrls: ['./weather-card.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class WeatherCardComponent implements OnInit {
+  @Input() isFeatured: boolean;
+  weatherTempState: string;
 
-  constructor() { }
+  constructor() {
+    this.isFeatured = false;
+  }
 
   ngOnInit() {
+    this.weatherTempState = `weather_hot`;
   }
 
 }
