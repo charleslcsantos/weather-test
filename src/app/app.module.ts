e3c2ca9c3c2ccfa9101956c949827033f3c5697e
Charles Lennon <charleslcsantos@gmail.com>
2018-05-19 00:47:34 -0300

    adding route defaul

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
 import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 
@@ -6,6 +7,11 @@ import { AppComponent } from './app.component';
 import { TopbarComponent } from './topbar/topbar.component';
 import { WeatherCardComponent } from './weather-card/weather-card.component';
 import { WeatherService } from './services/Weather/weather.service';
+
+const appRoutes: Routes = [
+  { path: '', component: AppComponent }
+];
+
 
 @NgModule({
   declarations: [
@@ -16,6 +22,7 @@ import { WeatherService } from './services/Weather/weather.service';
   imports: [
     BrowserModule,
     HttpClientModule,
+    RouterModule.forRoot(appRoutes, { enableTracing: false }),
   ],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
   providers: [
